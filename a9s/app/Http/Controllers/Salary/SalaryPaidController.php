b74baabadf16b86c5d7fd1422bfcd1fe9028192d
chandra <chandra>
2024-10-24 17:25:01 +0700

    temp

--- a9s/app/Http/Controllers/Salary/SalaryPaidController.php
+++ a9s/app/Http/Controllers/Salary/SalaryPaidController.php
@@ -682,7 +682,8 @@ class SalaryPaidController extends Controller
     $sts = StandbyTrx::where('created_at',"<=",$model_query->period_end." 23:59:59")
     ->where('val2',1)->whereNull('salary_paid_id')->where('req_deleted',0)->where('deleted',0)->with(['details'=>function ($q){
       $q->where("be_paid",1);      
-    }])->lockForUpdate()->get();
+    }])->
+    where("standby_mst_id","!=",11)->lockForUpdate()->get();
 
     foreach ($sts as $k => $v) {
       $smd = $v->standby_mst->details;
